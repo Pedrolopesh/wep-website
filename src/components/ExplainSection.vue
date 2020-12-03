5564dcb4ab1174cbb3b36c6bbf5dbf9d9de65b3a
Pedro Lopes <pedro@stratum.bt>
2020-12-03 17:50:07 -0300

    fix on env ans styles

--- src/components/ExplainSection.vue
+++ src/components/ExplainSection.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="text_section margin_section">
+        <div class="text_section margin_section p15">
             <h1 class="alg-txt-c mt-10">Um app de EVENTOS?</h1>
             <p class="alg-txt-c font-size-20 mt-3">Exatamente, um aplicativo de eventos com muitas possibilidades, além de saber o que ta rolando pela sua régião, é possível você criar seu próprio evento!</p>
         </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
     
-      <div class="hiden-music" v-bind="showMusic">
-          <audio controls autoplay>
+
+    <CoverSection/>
+
+    <!-- class="hiden-music" -->
+      <div class="custom-music p15" v-bind="showMusic">
+        <h2 class="alg-txt-c"> Colocamos uma música para melhorar suas experiência em nosso site, se quiser pode pausar 😎</h2>
+          <audio class="ac d-block" controls autoplay>
               <source src="../assets/audio.mp3" type="audio/mpeg">
               <source src="../assets/audio.mp3" type="audio/ogg">
             </audio>
             <embed height="1" type="audio/midi" width="144" src="../assets/audio.mp3" volume="10" loop="true" autostart="true" />        
       </div>
 
-    <CoverSection/>
     <ExplainSection/>
     <ExplainAppSection/>
     <LocalSection />
@@ -50,6 +54,15 @@ export default {
 }
 </script>
 <style lang="scss">
+.custom-music{
+  margin-top: 50px;
+  max-width: 100%;
+  width: 100%;
+
+  audio{
+    margin-top: 20px;
+  }
+}
   .hiden-music{
     display: none;
   }
